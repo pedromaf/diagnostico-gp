827c097e364d026a819dcab23b438bf685815ec3
Pedro Marinho <pmaf@ic.ufal.br>
2024-09-28 17:49:42 -0300

    [Backend] Adding initial files.

---
+++ backend/src/test/java/com/gp/diagnostico/DiagnosticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gp.diagnostico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
